83eed88bdab2cb6173126223c889b98776bbe1d7
RishavShah03 <rishav@fabstudio.co>
2022-04-18 12:33:30 +0530

    nvm commands to install, list and use node versions

---
+++ 2021/Git Commands/gitCommand.js
@@ -0,0 +1,11 @@
+// nvm uses
+// list all the node package available
+-> nvm ls
+
+// install a spesific node package
+-> nvm install _______ (version name)
+
+// use to a spesific version to use
+-> nvm use _____ (version name)         
+// sometime you might get permission denied error while switching so run CMD using administrative 
+// permission and do it there
